43059575153451b315448b33e8a64b1dc55c73ba
Aaron Rohrbacher and Nate McGregor <aaron@>
2017-08-16 09:45:31 -0700

    create README file

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 function yearInput(year) {
   if((year % 4 === 0 || year % 400 === 0) && year % 100 !== 0) {
     return true;
-  }else if(year % 100 === 0){
+  }else {
     return false;
   }
   // if(year % 4 > 0 || year % 100 === 0) {
