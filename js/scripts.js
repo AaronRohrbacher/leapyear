774b08a5d1bf67940290797b254ad1a2e838c0f2
Aaron Rohrbacher and Nate McGregor <aaron@>
2017-08-16 09:43:20 -0700

    return true for years divisible by 400

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 
 function yearInput(year) {
-  if(year % 4 > 0 || year % 100 === 0) {
-    return false;
-  }else if (year % 4 === 0) {
+  if((year % 4 === 0 || year % 400 === 0) && year % 100 !== 0) {
     return true;
+  }else if(year % 100 === 0){
+    return false;
   }
+  // if(year % 4 > 0 || year % 100 === 0) {
+  //   return false;
+  // }else if (year % 4 === 0 || year % 400 === 0) {
+  //   return true;
+  // }
 }
 
 
